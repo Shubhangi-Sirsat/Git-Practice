8b6958a16026728d3360a9e1dcff4b846a41d1fa
Shubhangi-Sirsat <sirsatshubhangi19.com>
2024-11-23 21:29:48 +0530

    This is commit for checking conflict

--- Script2.sh
+++ Script2.sh
@@ -9,4 +9,4 @@ echo "Hello value of a is $a and value of b is $b"
 echo "Do you understand the script?"
 echo "This is updated comment"
 echo "Updated on 18/11/2024"
-
+echo "Adding a line to create merge conflict"
