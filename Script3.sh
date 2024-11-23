bd0f3749c24006773fadc927e9faf2a8823981e3
Shubhangi-Sirsat <sirsatshubhangi19.com>
2024-11-23 17:46:46 +0530

    addes all scripts in branch1

---
+++ Script3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#this is to check if the shell scripts picks up env variables
+echo "This is error script"   # sample script
+echo "This is my system path $PATH"
+
+#Set a JDK_HOME env variable
+export JDK_HOME=/bin/jdk
+echo "my new JDK home is=$JDK_HOME"
+echo "Adding new updates to script 3"
